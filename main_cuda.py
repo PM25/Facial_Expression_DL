938bca8ee5eb7bda3d7e9257ed25c03f133e54a7
Einz <jason199786109@gmail.com>
2019-11-24 21:47:31 +0800

    Change main.py to main_cuda.py

---
+++ main_cuda.py
@@ -0,0 +1,43 @@
+from mylib.dataloader import DataLoader
+
+import torch
+from argparse import ArgumentParser
+
+
+# Arguments
+parser = ArgumentParser(description='Testing ResNet Model')
+parser.add_argument('--model', type=str, default='models/model_cuda.pkl', help='Path of Previous Trained Model')
+parser.add_argument('--imgs', type=str, default='data/images', help='Path of Testing Images')
+parser.add_argument('--labels', type=str, default='data/labels.txt', help='Path of Labels File')
+parser.add_argument('--bs', default=32, type=int, help='Batch Size')
+args = parser.parse_args()
+
+
+# Start frome here!
+if __name__ == '__main__':
+    # Testing Data
+    data = DataLoader(imgs_dir=args.imgs, labels_path=args.labels, batch_sz=args.bs)
+    test_loader = data.test_loader()
+    n_classes = data.n_classes
+    classes = ["Surprise", "Fear", "Disgust", "Happiness", "Sadness" ,"Anger", "Neutral"]
+
+    # Load Model
+    model = torch.load(args.model)
+
+    # Evaluation
+    class_correct = [ 0. for i in range(n_classes) ]
+    class_total = [ 0. for i in range(n_classes) ]
+    with torch.no_grad():
+        for batch_X, batch_y in test_loader:
+            outputs = model(batch_X.cuda())
+            _, predicts = torch.max(outputs, 1)
+            correct = (predicts == batch_y.cuda()).squeeze().tolist()
+            for label, c in zip(batch_y, correct):
+                class_correct[label] += c
+                class_total[label] += 1
+    
+    print('-' * 10)
+    for i in range(n_classes):
+        print(f"Test Accuracy of {classes[i]}: {100*(class_correct[i]/class_total[i]):.2f}%")
+    print('-' * 10)
+    print(f'Overall Accuracy: {100*(sum(class_correct)/sum(class_total)):.2f}%')
